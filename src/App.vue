376f50f46af93fb5da4d1ec2ddc46838a2dd9ee1
Brock37 <remondflorent@gmail.com>
2019-09-19 10:19:33 +0200

    Init component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WeekMeal/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeekMeal from './components/WeekMeal.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WeekMeal
   }
 }
 </script>
---
+++ src/components/WeekMeal.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="weekmeal">
+    <table class="ui celled padded table">
+      <thead>
+        <tr>
+          <th colspan="8" class="ui center aligned header">Menu de la semaine</th>
+        </tr>
+        <tr>
+          <th></th>
+          <th v-for="jour in JourSemaine" class="center aligned"> {{ jour }}</th>
+        </tr>
+
+      </thead>
+      <tbody>
+        <tr v-for="x in 2">
+          <td class="center aligned"> {{ repas[x-1] }}</td>
+          <td v-for="y in 7">
+
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+
+
+  data(){
+    return{
+      JourSemaine: [
+        "Lundi",
+        "Mardi",
+        "Mercredi",
+        "Jeudi",
+        "Vendredi",
+        "Samedi",
+        "Dimanche"
+      ],
+      repas: [ "Midi", "Soir"]
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
