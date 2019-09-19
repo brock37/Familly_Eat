03f0569b37fc9eeddff284bb057a9b0ee4053fda
Brock37 <remondflorent@gmail.com>
2019-09-19 09:45:09 +0200

    Add files

---
+++ src/components/Meal.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="main">
+    <ul>
+      <li>Id : {{ meal.id }}</li>
+      <li>Nom : {{ meal.nom }}</li>
+      <li>Temps de préparation : {{ meal.prepareTime }}</li>
+      <li>Temps de cuisson : {{ meal.cookTime }}</li>
+      <div class="ui right floated small primary icon button" @click.prevent="changeMeal(meal)">
+        <i class="exchange icon"></i>
+      </div>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props:{
+    meal: {
+      type: Object,
+      required: true
+    }
+  },
+    methods:{
+      changeMeal(meal){
+        this.$emit("change-meal", meal)
+      }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
