c74f1be6b98a2715de6d12f213e466739c690bb6
Brock37 <remondflorent@gmail.com>
2019-09-19 11:56:16 +0200

    create table from recettes.json

--- src/components/WeekMeal.vue
+++ src/components/WeekMeal.vue
@@ -15,7 +15,7 @@
         <tr v-for="x in 2">
           <td class="center aligned"> {{ repas[x-1] }}</td>
           <td v-for="y in 7">
-
+            <Meal :meal="mealForWeek[(y-1)+(x-1)*6]"/>
           </td>
         </tr>
       </tbody>
@@ -24,12 +24,48 @@
 </template>
 
 <script>
+import Meal from './Meal.vue'
+import BookMeal from '../assets/recettes.json'
 export default {
 
+  components:
+  {
+    Meal
+  },
   data(){
     return{
+      bookMeal : BookMeal,
+      mealForWeek : [],
       JourSemaine: ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"],
       repas: [ "Midi", "Soir"]
+    }
+  },
+  methods :{
+    randomIndex(){
+      return Math.floor(Math.random() * this.bookMeal.length)
+    },
+    randomMeal(id){
+      return this.bookMeal[this.randomIndex()]
+    },
+    inArray(id){
+      let res= false
+      for(var x = 0; x<this.mealForWeek.length; x++ ){
+        if(this.mealForWeek[x].id == id){
+          res= true
+        }
+      }
+      return res
+    }
+  },
+  created(){
+    for(let i= 0; i <13; i++){
+      let tmp =this.randomMeal()
+      //On evite les doublons de recette dans la semaine
+      do{
+        tmp= this.randomMeal()
+      }while(this.inArray(tmp.id))
+
+      this.mealForWeek.push(tmp)
     }
   }
 
