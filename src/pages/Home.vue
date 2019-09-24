719e6e01310a43a55eec1b225c2be9b708894024
Brock37 <remondflorent@gmail.com>
2019-09-24 16:41:31 +0200

    Complete home page with main layout and table of meals

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,17 @@
 <template lang="html">
+  <main-layout>
+    <WeekMeal/>
+  </main-layout>
 
 </template>
 
 <script>
+import MainLayout from "../layout/Main.vue"
+
 export default {
+  components : {
+    MainLayout
+  }
 }
 </script>
 
