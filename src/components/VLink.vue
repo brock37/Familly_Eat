cd8d801c5fbc9bb5fd388d9eb0961570a42326d6
Brock37 <remondflorent@gmail.com>
2019-09-24 16:40:12 +0200

    init Vlink component

---
+++ src/components/VLink.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <a v-bind:href="href"
+     v-bind:class="{active :isActive}"
+     v-on:click.prevent="go"
+
+  ></a>
+
+</template>
+
+<script>
+import routes from "../routes.js"
+export default {
+  props:{
+    href:{
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.href === this.$root.currentRoute
+    }
+  },
+  methods: {
+    go (event) {
+      this.$root.currentRoute = this.href
+      window.history.pushState(
+        null,
+        route[this.href],
+        this.href
+      )
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
