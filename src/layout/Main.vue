69a3e6fabe0a9c790de8dbc9d610d738cb2d0539
Brock37 <remondflorent@gmail.com>
2019-09-24 16:41:05 +0200

    Complete main layout with link for navigation in app

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,9 +1,25 @@
 <template lang="html">
+  <div class="ui center aligned container">
+    <ul>
+      <li>
+        <v-link href="/">Home</v-link>
+        <v-link href="/add">AJouter recette</v-link>
+      </li>
+    </ul>
+
+    <slot></slot>
+  </div>
+
 
 </template>
 
 <script>
+import VLink from '../components/VLink.vue'
+
 export default {
+  components:{
+    VLink
+  }
 }
 </script>
 
