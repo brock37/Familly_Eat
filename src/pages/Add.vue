4338d0d4ee35fc0c8382970143b4d194f5246b13
Brock37 <remondflorent@gmail.com>
2019-09-26 15:49:21 +0200

    use find savefile function

--- src/pages/Add.vue
+++ src/pages/Add.vue
@@ -63,6 +63,17 @@ export default {
     }
   },
   methods : {
+    saveFile: function() {
+      const data = JSON.stringify(this.cookBook)
+      const blob = new Blob([data], {type: 'text/plain'})
+      const e = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+      a.download = "recettes.json";
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
+    },
     submitMeal(){
       if(this.nom === ""){
         console.log("Pas de nom")
