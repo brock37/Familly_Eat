05ad4ffba2af13c9fba0ed5fe03549d19c97f051
Brock37 <remondflorent@gmail.com>
2019-09-26 10:21:46 +0200

    insert correctly weekmeal components to home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,19 @@
 <template lang="html">
+<div class="">
   <main-layout>
-    <WeekMeal/>
   </main-layout>
-
+  <WeekMeal/>
+</div>
 </template>
 
 <script>
 import MainLayout from "../layout/Main.vue"
+import WeekMeal from "../components/WeekMeal.vue"
 
 export default {
   components : {
-    MainLayout
+    MainLayout,
+    WeekMeal
   }
 }
 </script>
