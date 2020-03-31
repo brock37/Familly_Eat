5acbcb8567a9809ebd59c99d4a6025a4af9afe83
Brock37 <remondflorent@gmail.com>
2020-03-31 22:35:50 +0200

    Add overlay to weekmeal component

--- src/components/WeekMeal.vue
+++ src/components/WeekMeal.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <div class="weekmeal ui center aligned segment container">
+    <Overlay v-if="showSearch" @valid-meal="changeMeal" @close-overlay="showSearch = false"/>
     <div class="ui divided list">
       <div class="item" v-for="jour in JourSemaine" v-bind:key="jour">
         <div class="ui header">{{ jour }}</div>
@@ -9,7 +10,9 @@
               <div class="item" v-for="r in repas" v-bind:key="r">
                 <div class="content">
                   <div class="header">{{ r }}</div>
-                  <Meal :meal="mealForWeek.mealSelected[JourSemaine.indexOf(jour) + repas.indexOf(r) * 7]" :idmealweek="JourSemaine.indexOf(jour) + repas.indexOf(r) * 7" @change-meal="changeMeal"/>
+                  <Meal :meal="mealForWeek.mealSelected[JourSemaine.indexOf(jour) + repas.indexOf(r) * 7]"
+                   :idmealweek="JourSemaine.indexOf(jour) + repas.indexOf(r) * 7"
+                   @show-overlay="showOverlay"/>
                 </div>
               </div>
             </div>
@@ -22,19 +25,23 @@
 
 <script>
 import Meal from './Meal.vue'
+import Overlay from './Overlay.vue'
 
 export default {
 
   components:
   {
-    Meal
+    Meal,
+    Overlay
   },
   data(){
     return{
       cookBook : this.$root.$data.store.state.cookBook,
       mealForWeek : this.$root.$data.store.state.mealForWeek,
       JourSemaine: ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"],
-      repas: [ "Midi", "Soir"]
+      repas: [ "Midi", "Soir"],
+      showSearch : false,
+      searchId : null
     }
   },
   methods :{
@@ -54,7 +61,12 @@ export default {
       return newMeal
     },
     changeMeal(arg){
-      this.mealForWeek.mealSelected.splice(arg[0],1,arg[1])
+      //this.mealForWeek.mealSelected.splice(arg[0],1,arg[1])
+      this.mealForWeek.mealSelected.splice(this.searchId,1,arg)
+    },
+    showOverlay(arg){
+      this.showSearch = true
+      this.searchId = arg
     }
   },
   created(){
